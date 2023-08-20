2911a9eae500108c56c650e50d3caeab8bf2c9e9
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2023-08-20 01:37:32 +0100

    Implemented bfs and used it to find shortest path in unweighted graph

--- graph/graph.go
+++ graph/graph.go
@@ -1,7 +1,11 @@
 package graph
 
 import (
+	"fmt"
+	"math"
+
 	sll "github.com/emirpasic/gods/lists/singlylinkedlist"
+	queue "github.com/emirpasic/gods/queues/arrayqueue"
 )
 
 // Adjacency list representation of a graph
@@ -22,4 +26,66 @@ func NewGraph(v int) *Graph {
 		v:             v,
 		AdjacencyList: adjacencyList,
 	}
+}
+
+func (g *Graph) BFS(root int) {
+
+	visited := make([]bool, g.v)
+	q := queue.New()
+	visited[root] = true
+	q.Enqueue(root)
+
+	for !q.Empty() {
+		curr, _ := q.Dequeue()
+		fmt.Printf("%d ", curr)
+		it := g.AdjacencyList[curr.(int)].Iterator()
+		for it.Next() {
+			if !visited[it.Value().(int)] {
+				visited[it.Value().(int)] = true
+				q.Enqueue(it.Value().(int))
+			}
+		}
+	}
+}
+
+// Shortest path for unweighted graph
+func (g *Graph) PrintShortestPath(srcNode, destNode int) {
+
+	q := queue.New()
+	visited := make([]bool, g.v)
+	dist, pred := make([]int, g.v), make([]int, g.v)
+	for i := 0; i < g.v; i++ {
+		dist[i] = math.MaxInt
+		pred[i] = -1
+	}
+
+	q.Enqueue(srcNode)
+	visited[srcNode] = true
+	dist[srcNode] = 0
+loop:
+	for !q.Empty() {
+		currNode, _ := q.Dequeue()
+		it := g.AdjacencyList[currNode.(int)].Iterator()
+		for it.Next() {
+			if !visited[it.Value().(int)] {
+				visited[it.Value().(int)] = true
+				dist[it.Value().(int)] = dist[currNode.(int)] + 1
+				pred[it.Value().(int)] = currNode.(int)
+				q.Enqueue(it.Value().(int))
+				if it.Value().(int) == destNode {
+					break loop
+				}
+			}
+		}
+	}
+
+	path := []int{}
+	crawl := destNode
+	path = append(path, crawl)
+	for pred[crawl] != -1 {
+		path = append(path, pred[crawl])
+		crawl = pred[crawl]
+	}
+	fmt.Println(pred, path)
+
 }
--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Arafetki/graph-theory/graph"
 )
 
 func main() {
 
 	// (Unweighted directed Graph) v0->v1 v0->v2 v1->v3 v3->v1 v2->v3
-	v := 4
+	v := 5
 	g := graph.NewGraph(v)
 	g.AdjacencyList[0].Add(1, 2)
-	g.AdjacencyList[1].Add(3)
-	g.AdjacencyList[2].Add(3)
-	g.AdjacencyList[3].Add(1)
-	for i := 0; i < v; i++ {
-		fmt.Println(g.AdjacencyList[i].Values())
-	}
+	g.AdjacencyList[1].Add(0, 3)
+	g.AdjacencyList[2].Add(0, 4)
+	g.AdjacencyList[3].Add(1, 4)
+	g.AdjacencyList[4].Add(2, 3)
+	g.PrintShortestPath(3, 0)
 }
